9b34665d1d6be9c34b7eb5f07d07ce7c9e36fc99
Luke Ferris <luke.ferris@gmail.com>
2023-09-14 13:39:15 +0200

    Shifted over to vite

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="App">Custom content goes here!</div>;
-}
-
-export default App;
---
+++ src/Main.jsx
@@ -0,0 +1,5 @@
+import "./App.css";
+
+export default function Main() {
+  return <div>HELLO WORLD</div>;
+}
