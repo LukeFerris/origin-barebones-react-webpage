2bc02c97f9d3646a80e21a0e1196476508c5c40b
Luke Ferris <luke.ferris@gmail.com>
2023-09-16 07:43:10 +0200

    Added alias for assets

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
   clearScreen: false,
   server: {
     port: 3001,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 });
