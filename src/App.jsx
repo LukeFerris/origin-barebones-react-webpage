b74b3dcc503907d2897d0e36a54afb8f73c4e6d2
Luke Ferris <luke.ferris@gmail.com>
2023-09-18 19:03:21 +0200

    Added theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Main from "./Main.jsx";
 import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Main />
+    <ThemeProvider theme={theme}>
+      <Main />
+    </ThemeProvider>
   </React.StrictMode>
 );
